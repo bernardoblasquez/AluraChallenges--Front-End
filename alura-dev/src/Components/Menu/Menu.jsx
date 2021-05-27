d5503b25eaaf1f8c619cd7ef048aaf3f9b1a326a
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-05-27 09:51:25 -0300

    refactor (Menu): retirada de estilo e elemento titulo de Menu

--- alura-dev/src/Components/Menu/Menu.jsx
+++ alura-dev/src/Components/Menu/Menu.jsx
@@ -6,33 +6,32 @@ import communityIcon from '../../Images/community-icon.svg';
 const menu = (props) => {
 
     return(
-        <>
-            <h3 className="menu__title">menu</h3>
-            <nav className="menu">
-                <ul className='menu__list'>
-                    <li className="menu__item">
-                        <a href="xvdvv">
-                            <MenuIcon 
-                                icon={codeIcon}
-                                href="sdfsdsdfs"
-                                text='Editor de código'
-                                alt='texto'
-                            />
-                        </a>
-                    </li>
-                    <li className="menu__item">
-                        <a href="sdads">
-                            <MenuIcon
-                                icon={communityIcon}
-                                href="sdfsdsdfs"
-                                text='Comunidade'
-                                alt='texto'
-                            />
-                        </a>
-                    </li>
-                </ul>
-            </nav>
-        </>
+        
+        <nav className="menu">
+            <ul className='menu__list'>
+                <li className="menu__item">
+                    <a href="xvdvv" className="menu__link">
+                        <MenuIcon 
+                            icon={codeIcon}
+                            href="sdfsdsdfs"
+                            text='Editor de código'
+                            alt='texto'
+                        />
+                    </a>
+                </li>
+                <li className="menu__item">
+                    <a href="sdads" className="menu__link">
+                        <MenuIcon
+                            icon={communityIcon}
+                            href="sdfsdsdfs"
+                            text='Comunidade'
+                            alt='texto'
+                        />
+                    </a>
+                </li>
+            </ul>
+        </nav>
+        
     )
 }
 
