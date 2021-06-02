4e19b2d279d98ef4a022e3217547ba3d5b9a8128
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-06-02 10:37:29 -0300

    feat: criação do componente FormProject e configuração de seus estados, que pegam o nome e a descrição do projeto

---
+++ alura-dev/src/Components/Forms/FormProject.jsx
@@ -0,0 +1,80 @@
+import { useState } from 'react'
+import './Form.scss';
+
+
+const FormProject = (props) => {
+
+    const projectNameText = 'Nome do seu projeto';
+    const projectDescriptionText = 'Descrição do seu projeto';
+
+    const [projectNameState, setProjectNameState] = useState(projectNameText);
+    const [projectNameFocusState, setProjectNameFocusState] = useState(false);
+    const [projectDescriptionState, setProjectDescriptionState] = useState(projectDescriptionText);
+    const [projectDescriptionFocusState, setProjectDescriptionFocusState] = useState(false);
+
+    const projectNameChangeHandler = (event) =>{
+        let projectName = event.target.value;
+         
+        setProjectNameState(projectName)
+    }
+
+    const projectNameFocusHandler = () =>{
+        if (projectNameFocusState === false){
+            setProjectNameState('')
+            setProjectNameFocusState(true)
+        }
+    }
+
+    const projectNameBlurHandler = () =>{
+        if (projectNameState === ''){
+            setProjectNameState(projectNameText)
+            setProjectNameFocusState(false)
+        }
+    }
+
+    const projectDescriptionChangeHandler = (event) =>{
+        let projectDescription = event.target.value;
+         
+        setProjectDescriptionState(projectDescription)
+    }
+
+    const projectDescriptionFocusHandler = (event) =>{
+        if (projectDescriptionFocusState === false){
+            setProjectDescriptionState('')
+            setProjectDescriptionFocusState(true)
+        }
+    }
+
+    const projectDescriptionBlurHandler = () =>{
+        if (projectDescriptionState === ''){
+            setProjectDescriptionState(projectDescriptionText)
+            setProjectDescriptionFocusState(false)
+        }
+    }
+
+    console.log(projectNameState)
+    console.log(projectDescriptionState)
+
+    return(
+        <div>
+            <input 
+                className="form-input"
+                value={projectNameState}
+                onChange={projectNameChangeHandler}
+                onFocus={projectNameFocusHandler}
+                onBlur={projectNameBlurHandler}
+                type="text" />
+            <textarea 
+                className="form-textarea" 
+                value={projectDescriptionState}
+                onChange={projectDescriptionChangeHandler}
+                onFocus={projectDescriptionFocusHandler}
+                onBlur={projectDescriptionBlurHandler}
+                name="" 
+                id="" 
+            />
+        </div> 
+    )
+}
+
+export default FormProject; 
