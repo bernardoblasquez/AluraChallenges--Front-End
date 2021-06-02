050b663c53333a4812ad702c1abc65c1ad4dc4f6
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-06-02 10:47:11 -0300

    feat: criação dos componentes para selecionar cor e linguagem do editor

---
+++ alura-dev/src/Components/Forms/FormSelectEditorColor.jsx
@@ -0,0 +1,10 @@
+
+const FormFieldColorSelector = () => {
+    return (
+        <div className="form-color-selector">
+            <div className="form-color-selector__color"></div>
+        </div>
+    )
+}
+
+export default FormFieldColorSelector
---
+++ alura-dev/src/Components/Forms/FormSelectLanguage.jsx
@@ -0,0 +1,12 @@
+import './Form.scss';
+
+
+const FormProjectConfig = () => {
+    return(
+        <div>
+            <select name="" id="" className="form-input"></select> 
+        </div> 
+    )
+}
+
+export default FormProjectConfig;
