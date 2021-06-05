aff4607ea8e95586b214fe45803bd744e7feb276
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-06-05 12:29:21 -0300

    refactor (Header): separação do código e estilo de busca em outro componente - HeaderSearch

--- alura-dev/src/Components/Header/Header.jsx
+++ alura-dev/src/Components/Header/Header.jsx
@@ -1,58 +1,20 @@
 //import styles from './Header.module.scss';
-
-import { useState } from 'react'
 import MenuDropdown from '../Menu/MenuDropdown'
+import HeaderSearch from './HeaderSearch';
 import './Header.scss';
 import logo from '../../Images/Logo.svg'
-import searchIcon from '../../Images/search-icon.svg' 
 import avatarIcon from '../../Images/Avatar.png' 
 
 const Header = () => {
 
-    const inputSearchText = 'Busque por Algo';
-    const [inputSearchSate, setInputSearchSate] = useState(inputSearchText)
-    const [inputSearchFocusSate, setInputSearchFocusSate] = useState(false)
-
-    const inputSearchChangeHandler = (event) => {
-        setInputSearchSate(event.target.value)
-        console.log(inputSearchSate)
-    }
-
-    const inputSearchFocusHandler = () => { 
-        if (inputSearchFocusSate === false){
-            setInputSearchSate('');
-            setInputSearchFocusSate(true);
-        }
-    }
-    
-    const inputSearchBlurHandler = () => { 
-        if (inputSearchSate === ''){
-            setInputSearchSate(inputSearchText);
-            setInputSearchFocusSate(false);
-        }
-    }
-
     return(
         <header className='header-app'>
-            <div className="header-logo">
-                <img src={logo} alt="Logo da Alura Dev" />
+            
+            <div className="header-logo"> 
+                <img src={logo} alt="Logo da Alura Dev" />  
             </div>
             
-            
-            <form action="" className="header-form" >
-                <input type="text"  
-                    className="header-form__search-input" 
-                    value={inputSearchSate}
-                    onChange={inputSearchChangeHandler}
-                    onFocus={inputSearchFocusHandler}
-                    onBlur={inputSearchBlurHandler}
-                />
-
-                <img src={searchIcon}
-                    className="header-form__search-icon" 
-                    alt="" 
-                />
-            </form>
+            <HeaderSearch defaultTxt='Busque por Algo'/>
 
             <MenuDropdown />
 
