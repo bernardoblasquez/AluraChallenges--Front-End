b3692a703229cf6a2da8f37d23236bfb4be5fb1f
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-06-08 08:55:48 -0300

    feat: adição de opções de linguagens, configuração de evento e passagem de linguaguem selecionada para o pai

--- alura-dev/src/Components/Forms/FormSelectLanguage.jsx
+++ alura-dev/src/Components/Forms/FormSelectLanguage.jsx
@@ -1,12 +1,27 @@
 import './Form.scss';
 
 
-const FormProjectConfig = () => {
+const FormSelectLanguage = (props) => {
+
+
+    const selectChangeHandler = (event) => {
+        //console.log(event.target.value)
+        props.onSelectLanguage(event.target.value)
+    }
+    
+
     return(
         <div>
-            <select name="" id="" className="form-input"></select> 
+            <select className="form-input"
+                onChange={selectChangeHandler}
+                name="" id="">
+                <option value="javascript">javascript</option>
+                <option value="css">css</option>
+                <option value="json">json</option>
+                <option value="html">html</option>
+            </select> 
         </div> 
     )
 }
 
-export default FormProjectConfig;
+export default FormSelectLanguage;
