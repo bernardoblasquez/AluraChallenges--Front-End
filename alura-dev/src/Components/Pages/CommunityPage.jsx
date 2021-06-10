56f204ba596bc068e3bcae29167711149c99ea22
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-06-10 09:39:59 -0300

    feat, style: criação do componente CommunityPage e estilização do grid

--- alura-dev/src/Components/Pages/CommunityPage.jsx
+++ alura-dev/src/Components/Pages/CommunityPage.jsx
@@ -1,10 +1,27 @@
 
+import Menu from '../Menu/Menu';
+import CodeCard from '../CodeEditor/CodeCard';
+import CodeCardList from '../CodeEditor/CodeCardList';
+
+import './PagesGrid.scss'
 
 const CommunityPage = () =>{
     return(
-        <main>
-            
-        </main>
+        <div className="grid-column">
+
+            <section className="grid-column__menu">
+                <h2 className="grid-column__title">
+                    Menu
+                </h2>
+                <Menu />
+            </section>
+
+            <div className="grid-column__community">
+                <CodeCardList />
+            </div>
+
+
+        </div>
     )
 }
 
