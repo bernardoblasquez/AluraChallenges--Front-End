e615df8eded07664530b3c05439e1a91136e647d
Bernardo (John.bt) <bernardo.bt@gmail.com>
2021-06-14 07:33:42 -0300

    feat: uso de códigos de diferentes linguahgens para testar o Syntax Highlighter em codeCard

--- alura-dev/src/Components/CodeEditor/CodeCardList.jsx
+++ alura-dev/src/Components/CodeEditor/CodeCardList.jsx
@@ -5,16 +5,83 @@ const CodeCardList = () => {
     return(
         <ul className="code-card-list">
             <li className="code-card-list__item">
-                <CodeCard />
+                <CodeCard labguage="javascript" bgColor="code-editor--bg-color-orange">
+                {`
+    class MyClass {
+        public static myValue: string;
+        constructor(init: string) {
+        this.myValue = init;
+        }
+    }
+    import fs = require("fs");
+    module MyModule {
+        export interface MyInterface extends Other {
+        myProperty: any;
+        }
+    }
+    declare magicNumber number;
+    myArray.forEach(() => { }); 
+                ` }
+                </CodeCard>
             </li>
             <li className="code-card-list__item">
-                  <CodeCard />
+                <CodeCard labguage="json" bgColor="code-editor--bg-color-blue">
+                {`
+    [
+        {
+            "title": "apples",
+            "count": [12000, 20000],
+            "description": {"text": "...", "sensitive": false}
+        },
+        {
+            "title": "oranges",
+            "count": [17500, null],
+            "description": {"text": "...", "sensitive": false}
+        }
+    ]
+                ` }
+                </CodeCard>
             </li>
             <li className="code-card-list__item">
-                <CodeCard />
+                <CodeCard labguage="css" bgColor="code-editor--bg-color-pink">
+                {`
+    @font-face {
+        font-family: Chunkfive; src: url('Chunkfive.otf');
+    }
+
+    body, .usertext {
+        color: #F0F0F0; background: #600;
+        font-family: Chunkfive, sans;
+        --heading-1: 30px/32px Helvetica, sans-serif;
+    }
+
+    @import url(print.css);
+    @media print {
+        a[href^=http]::after {
+            content: attr(href)
+    }
+                `}
+
+                </CodeCard>
             </li>
             <li className="code-card-list__item">
-                <CodeCard />
+                <CodeCard labguage="html" bgColor="code-editor--bg-color-green">
+                    {`  
+    <!DOCTYPE html>
+    <title>Title</title>
+    
+    <style>body {width: 500px;}</style>
+    
+    <script type="application/javascript">
+      function $init() {return true;}
+    </script>
+    
+    <body>
+      <p checked class="title" id='title'>Title</p>
+      <!-- here goes the rest of the page -->
+    </body>
+                    `}
+                </CodeCard>
             </li>
         </ul>
     )
