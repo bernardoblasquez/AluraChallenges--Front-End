c990877e8c37275aebb0dc635439438c8193caf1
Bernardo <bernardo.bt@gmail.com>
2021-06-15 10:12:41 -0300

    feat: formatação do Json e update da lista de códigos

--- alura-dev/src/App.js
+++ alura-dev/src/App.js
@@ -1,4 +1,5 @@
 import { Route, Switch, Redirect } from 'react-router-dom';
+import { useState } from 'react'
 import Header from './Components/Header/Header'
 import CodeEditorForm from './Pages/CodeEditorForm';
 import CodeCommunity from './Pages/CodeCommunity';
@@ -12,94 +13,52 @@ const CODES = [
       title: "Um código Javascript",
       description: "Um exemplo de código utilizando a linguagem Javascript",
       language: "javascript",
-      bgColor: "bg-color-blue",
+      bgColorClass: "bg-color-blue",
       totalOfComments: 31,
       totalOfLikes: 9,
-      codeText:`    class MyClass {
-          public static myValue: string;
-          constructor(init: string) {
-          this.myValue = init;
-          }
-      }
-      import fs = require("fs");
-      module MyModule {
-          export interface MyInterface extends Other {
-          myProperty: any;
-          }
-      }
-      declare magicNumber number;
-      myArray.forEach(() => { }); 
-                  `
+      codeText: "    class MyClass {\n          public static myValue: string;\n          constructor(init: string) {\n          this.myValue = init;\n          }\n      }\n      import fs = require(\"fs\");\n      module MyModule {\n          export interface MyInterface extends Other {\n          myProperty: any;\n          }\n      }\n      declare magicNumber number;\n      myArray.forEach(() => { }); \n                  "
   },
   {
       title: "Um código Json",
       description: "Um exemplo de código utilizando o JSON",
       language: "json",
-      bgColor: "bg-color-green",
+      bgColorClass: "bg-color-green",
       totalOfComments: 18,
       totalOfLikes: 40,
-      codeText:`     [
-          {
-              "title": "apples",
-              "count": [12000, 20000],
-              "description": {"text": "...", "sensitive": false}
-          },
-          {
-              "title": "oranges",
-              "count": [17500, null],
-              "description": {"text": "...", "sensitive": false}
-          }
-      ]`
+      codeText: "     [\n          {\n              \"title\": \"apples\",\n              \"count\": [12000, 20000],\n              \"description\": {\"text\": \"...\", \"sensitive\": false}\n          },\n          {\n              \"title\": \"oranges\",\n              \"count\": [17500, null],\n              \"description\": {\"text\": \"...\", \"sensitive\": false}\n          }\n      ]"
   },
   {
       title: "CSS",
       description: "Estilo aleatório para teste",
       language: "css",
-      bgColor: "bg-color-pink",
+      bgColorClass: "bg-color-pink",
       totalOfComments: 5,
       totalOfLikes: 3,
-      codeText:`      @font-face {
-          font-family: Chunkfive; src: url('Chunkfive.otf');
-      }
-  
-      body, .usertext {
-          color: #F0F0F0; background: #600;
-          font-family: Chunkfive, sans;
-          --heading-1: 30px/32px Helvetica, sans-serif;
-      }
-  
-      @import url(print.css);
-      @media print {
-          a[href^=http]::after {
-              content: attr(href)
-      }`
+      codeText: "      @font-face {\n          font-family: Chunkfive; src: url('Chunkfive.otf');\n      }\n  \n      body, .usertext {\n          color: #F0F0F0; background: #600;\n          font-family: Chunkfive, sans;\n          --heading-1: 30px/32px Helvetica, sans-serif;\n      }\n  \n      @import url(print.css);\n      @media print {\n          a[href^=http]::after {\n              content: attr(href)\n      }"
   },
   {
       title: "HTML5",
       description: "Estruturando uma código de forma semantica",
       language: "html",
-      bgColor: "bg-color-orange",
+      bgColorClass: "bg-color-orange",
       totalOfComments: 20,
       totalOfLikes: 10,
-      codeText:`     
-      <!DOCTYPE html>
-      <title>Title</title>
-      
-      <style>body {width: 500px;}</style>
-      
-      <script type="application/javascript">
-        function $init() {return true;}
-      </script>
-      
-      <body>
-        <p checked class="title" id='title'>Title</p>
-        <!-- here goes the rest of the page -->
-      </body>`
+      codeText: "     \n      <!DOCTYPE html>\n      <title>Title</title>\n      \n      <style>body {width: 500px;}</style>\n      \n      <script type=\"application/javascript\">\n        function $init() {return true;}\n      </script>\n      \n      <body>\n        <p checked class=\"title\" id='title'>Title</p>\n        <!-- here goes the rest of the page -->\n      </body>"
   },
 
 ];
 
 function App() {
+
+  const [listOfCodes, setListOfCode] = useState(CODES);
+
+  const updateListOfCodesHandler = (newCode) => {
+
+    setListOfCode((prevListOfCodes) => {
+      return [...prevListOfCodes, newCode]
+    })
+  }
+
   return (
     <>
       <Header />
@@ -111,11 +70,11 @@ function App() {
           </Route>
 
           <Route path="/editor">
-            <CodeEditorForm />
+            <CodeEditorForm onSaveNewCode={updateListOfCodesHandler} />
           </Route>
 
           <Route exact path="/comunidade">
-            <CodeCommunity listOfCodes={CODES} />
+            <CodeCommunity listOfCodes={listOfCodes} />
           </Route>
 
           <Route path="/comunidade/:codeId">
